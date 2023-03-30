cf488878bef484af6340f71b52e835a0230d6f50
HungYs <viethung020996@gmail.com>
2023-03-30 23:18:00 +0900

    2023-03-30 Connect to Mysql

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
+
 require("dotenv").config();
 
 let app = express ();
@@ -9,11 +11,13 @@ let app = express ();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended : true
-}))
+})) 
 
-viewEngine(app)
+viewEngine(app);
 
-initWebRoutes(app)
+initWebRoutes(app);
+
+connectDB();
 
 let port = process.env.PORT || 8081;
 // PORT = undefined => port = 8081
